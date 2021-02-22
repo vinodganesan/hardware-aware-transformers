75c1f5c6df8d2d5d0318908615a28293303f25c5
vinodganesan <g.vinod1993@gmail.com>
2021-02-22 23:19:56 +0530

    Pushing latest FairSeq with TPU support

--- fairseq/modules/layer_norm.py
+++ fairseq/modules/layer_norm.py
@@ -1,21 +1,38 @@
-# HAT: Hardware-Aware Transformers for Efficient Natural Language Processing
-# Hanrui Wang, Zhanghao Wu, Zhijian Liu, Han Cai, Ligeng Zhu, Chuang Gan and Song Han
-# The 58th Annual Meeting of the Association for Computational Linguistics (ACL), 2020.
-# Paper: https://arxiv.org/abs/2005.14187
-# Project page: https://hanruiwang.me/project_pages/hat/
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
-def LayerNorm(normalized_shape, eps=1e-5, elementwise_affine=True, export=False):
-    if not export and torch.cuda.is_available():
-        try:
-            from apex.normalization import FusedLayerNorm
-            return FusedLayerNorm(normalized_shape, eps, elementwise_affine)
-        except ImportError:
-            pass
-    return torch.nn.LayerNorm(normalized_shape, eps, elementwise_affine)
 
+try:
+    from apex.normalization import FusedLayerNorm as _FusedLayerNorm
+
+    has_fused_layernorm = True
+
+    class FusedLayerNorm(_FusedLayerNorm):
+        @torch.jit.unused
+        def forward(self, x):
+            if not x.is_cuda:
+                return super().forward(x)
+            else:
+                with torch.cuda.device(x.device):
+                    return super().forward(x)
+
+
+except ImportError:
+    has_fused_layernorm = False
+
+
+def LayerNorm(normalized_shape, eps=1e-5, elementwise_affine=True, export=False):
+    if torch.jit.is_scripting():
+        export = True
+    if not export and torch.cuda.is_available() and has_fused_layernorm:
+        return FusedLayerNorm(normalized_shape, eps, elementwise_affine)
+    return torch.nn.LayerNorm(normalized_shape, eps, elementwise_affine)
 
 class LayerNormSuper(torch.nn.LayerNorm):
     def __init__(self, super_embed_dim):
@@ -55,3 +72,17 @@ class LayerNormSuper(torch.nn.LayerNorm):
         assert 'weight' in self.samples.keys()
         assert 'bias' in self.samples.keys()
         return self.samples['weight'].numel() + self.samples['bias'].numel()
+
+class Fp32LayerNorm(nn.LayerNorm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input):
+        output = F.layer_norm(
+            input.float(),
+            self.normalized_shape,
+            self.weight.float() if self.weight is not None else None,
+            self.bias.float() if self.bias is not None else None,
+            self.eps,
+        )
+        return output.type_as(input)
--- fairseq/optim/bmuf.py
+++ fairseq/optim/bmuf.py
@@ -3,10 +3,13 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from dataclasses import dataclass, field
+
 import torch
 import torch.distributed as dist
-
-from . import FairseqOptimizer
+from fairseq.dataclass.configs import FairseqBMUFConfig
+from fairseq.dataclass.utils import gen_parser_from_dataclass
+from fairseq.optim.fairseq_optimizer import FairseqOptimizer
 
 
 class FairseqBMUF(FairseqOptimizer):
@@ -19,49 +22,24 @@ class FairseqBMUF(FairseqOptimizer):
     model-update filtering
     """
 
-    def __init__(self, args, optimizer):
-
-        super().__init__(args)
+    def __init__(self, cfg: FairseqBMUFConfig, optimizer):
+        super().__init__(cfg)
         self._optimizer = optimizer
         self._num_updates = 0
-        self.sync_iter = self.args.global_sync_iter
-        self.block_momentum = self.args.block_momentum
-        self.block_lr = self.args.block_lr
+        self.sync_iter = cfg.global_sync_iter
+        self.block_momentum = cfg.block_momentum
+        self.block_lr = cfg.block_lr
         self._reset_local_data()
-        self.warmup_iteration = self.args.warmup_iterations
-        self.use_nbm = self.args.use_nbm
+        self.warmup_iteration = cfg.warmup_iterations
+        self.use_nbm = cfg.use_nbm
         self.initial_state = self._optimizer.state_dict()
+        self.average_sync = self.cfg.average_sync
+        self.world_size = self.cfg.distributed_world_size
 
     @staticmethod
     def add_args(parser):
         """Add optimizer-specific arguments to the parser."""
-        parser.add_argument(
-            "--block-lr", default=1, type=float, help="block learning rate for bmuf"
-        )
-        parser.add_argument(
-            "--block-momentum",
-            default=0.875,
-            type=float,
-            help="block momentum for bmuf",
-        )
-        parser.add_argument(
-            "--global-sync-iter",
-            default=10,
-            type=int,
-            help="Iteration for syncing global model",
-        )
-        parser.add_argument(
-            "--warmup-iterations",
-            default=500,
-            type=int,
-            help="warmup iterations for model to broadcast",
-        )
-        parser.add_argument(
-            "--use-nbm",
-            default=True,
-            action="store_true",
-            help="Specify whether you want to use classical BM / Nesterov BM",
-        )
+        gen_parser_from_dataclass(parser, FairseqBMUFConfig())
 
     @property
     def optimizer(self):
@@ -82,43 +60,66 @@ class FairseqBMUF(FairseqOptimizer):
 
     def load_state_dict(self, state_dict, optimizer_overrides=None):
         self._optimizer.load_state_dict(state_dict, optimizer_overrides)
+        self.initial_state = self._optimizer.state_dict()
 
     def multiply_grads(self, c):
         """Multiplies grads by a constant *c*."""
         self._optimizer.multiply_grads(c)
 
-    def clip_grad_norm(self, max_norm):
+    def clip_grad_norm(self, max_norm, aggregate_norm_fn=None):
         """Clips gradient norm."""
-        return self._optimizer.clip_grad_norm(max_norm)
+        return self._optimizer.clip_grad_norm(max_norm, aggregate_norm_fn)
+
+    def average_params(self):
+        self._optimizer.average_params()
 
     def _block_sync(self):
-        # Update the global model using local models from all GPUs.
-        if self._is_bmuf_iter():
-            if self.block_momentum != 0:
-                self._BM_before_sync()
+        if self.world_size <= 1:
+            return
+        # Update the global model using local models from all GPUs
+        # (Step-1) Calculate grad between previously synced model and
+        # currrent local model
+        if self.block_momentum != 0:
+            self._calc_grad()
 
-            self._allreduce_parameter()
+        # (Step-2) Average gradient from all GPUs
+        self._avg_grad_from_all_gpus()
 
-            if self.block_momentum != 0:
-                self._BM_after_sync()
+        # (Step-3) Calculate global momentum and update the global model
+        if self.block_momentum != 0:
+            self._update_global_model()
+
+        # (Step-4) Average local optimizer params
+        if self.average_sync:
+            self.average_params()
 
     def _is_warmup_end(self):
+        # Check whether train iterations is equal to warmup iter
         if self.get_num_updates() == self.warmup_iteration:
             return True
         return False
 
     def _is_bmuf_iter(self):
-        if self.get_num_updates() % self.sync_iter == 0:
+        # Check whether train iterations is equal to bmuf sync iter
+        if (self.get_num_updates() > self.warmup_iteration) and (
+            self.get_num_updates() % self.sync_iter == 0
+        ):
             return True
         return False
 
-    def _warmup_sync(self, rootRank=0):
-        # broadcast the local model to all GPUs
+    def _warmup_sync(self, root_rank=0):
+        if self.world_size <= 1:
+            return
+        # Broadcast the local model to all gpus
         for param in self.params:
-            dist.broadcast(param.data, src=rootRank)
+            dist.broadcast(param.data, src=root_rank)
 
-        # Reset the local optimizer state and local bmuf related param
-        self._optimizer.load_state_dict(self.initial_state)
+        # Update local optimizer state
+        if self.average_sync:
+            self._optimizer.average_params()
+        else:
+            self._optimizer.load_state_dict(self.initial_state)
+
         self._reset_local_data()
 
     def step(self, closure=None):
@@ -127,7 +128,7 @@ class FairseqBMUF(FairseqOptimizer):
         self.set_num_updates(self.get_num_updates() + 1)
         if self._is_warmup_end():
             self._warmup_sync()
-        else:
+        elif self._is_bmuf_iter():
             self._block_sync()
 
     def zero_grad(self):
@@ -144,61 +145,56 @@ class FairseqBMUF(FairseqOptimizer):
 
     @torch.no_grad()
     def _reset_local_data(self):
-        """Resetting all the BMUF specific params."""
-        self.params_localprev = [torch.zeros_like(p.data) for p in self.params]
-
-        self.smoothed_grads_localprev = [
-            p.data.new_zeros(p.data.size()) for p in self.params
-        ]
-        self.grads_localprev = [p.data.new_zeros(p.data.size()) for p in self.params]
+        # (Step-0) Initialize global momentum parameters and store global copy on each gpu
+        self.global_params = [torch.zeros_like(p.data) for p in self.params]
+        self.smoothed_grads = [p.data.new_zeros(p.data.size()) for p in self.params]
+        self.grads = [p.data.new_zeros(p.data.size()) for p in self.params]
 
         # saving the global model locally for calculating gradient during bmuf sync
-        for param, copy_param in zip(self.params, self.params_localprev):
-            copy_param.copy_(param.data)
+        for param, global_param in zip(self.params, self.global_params):
+            global_param.copy_(param.data)
 
     @torch.no_grad()
-    def _BM_before_sync(self):
-        """Calculate grad between previously synced model and currrent local model."""
-        # prev_param is basically the global copy from the previously finished
+    def _calc_grad(self):
+        # global_params is basically the global copy from the previously finished
         # synchronisation. param.data is local parameter after block_sync_freq
         # for the local gpu. so grad is difference between previously synced
         # model and currrent local model.
-        for index, (param, prev_param) in enumerate(
-            zip(self.params, self.params_localprev)
+        for index, (param, global_param) in enumerate(
+            zip(self.params, self.global_params)
         ):
-            self.grads_localprev[index] = prev_param - param.data
+            self.grads[index] = global_param - param.data
 
-    def _allreduce_parameter(self):
-        """Average gradient from all the GPUs. """
+    def _avg_grad_from_all_gpus(self):
         for index, param in enumerate(self.params):
-            sync_para = (
-                param.data if self.block_momentum == 0 else self.grads_localprev[index]
-            )
+            sync_para = param.data if self.block_momentum == 0 else self.grads[index]
             sync_para /= float(dist.get_world_size())
             dist.all_reduce(sync_para, op=dist.ReduceOp.SUM)
 
     @torch.no_grad()
-    def _BM_after_sync(self):
-        for index, (param, prev_param, smoothed_grad, grad) in enumerate(
+    def _update_global_model(self):
+        for index, (param, global_param, smoothed_grad, grad) in enumerate(
             zip(
                 self.params,
-                self.params_localprev,
-                self.smoothed_grads_localprev,
-                # all machines would share the same value of smoothed_grad, since it is
+                self.global_params,
+                self.smoothed_grads,
+                # all gpus would share the same value of smoothed_grad, since it is
                 # always computed on synchronized gradients.
-                self.grads_localprev,
+                self.grads,
             )
         ):
-            # prev_param is basically last syncrhornized parameter. though
+            # global_param is basically last syncrhornized parameter. though
             # smoothed_grad is local, all processes will have same value of
             # smoothed_grad and hence param is globally synchronized copy.
-            # smoothed_grad(t)=BM * smoothed_grad(t-1) + BM_lr*grad(t)
-            smoothed_grad = smoothed_grad * self.block_momentum + grad * self.block_lr
-            param.data.copy_(prev_param - smoothed_grad)
+            # smoothed_grad(t) = BM * smoothed_grad(t-1) + BM_lr * grad(t)
+            smoothed_grad = self.block_momentum * smoothed_grad + self.block_lr * grad
+            param.data.copy_(global_param - smoothed_grad)
+
             # A Nesterov momentum here is to do a partial weight update before
             # calculating the gradient
             if self.use_nbm:
                 param.data.copy_(param.data - self.block_momentum * smoothed_grad)
+
             # backup for the next synchronization.
-            self.smoothed_grads_localprev[index] = smoothed_grad
-            prev_param.copy_(param.data)
+            self.smoothed_grads[index] = smoothed_grad
+            global_param.copy_(param.data)
